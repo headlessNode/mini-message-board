1793cf8085d93ba51a7387d5dcc5d50fa83afd38
headlessNode <harriskhan047@outlook.com>
2024-10-04 16:25:01 +0500

    feat: Add views, message form and setup routes

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require( 'express' );
+const ejs = require( 'ejs' );
+const path = require( 'node:path' );
+const indexRouter = require('./routes/indexRouter.js');
+const newMessageRouter = require( './routes/newMessageRouter.js' );
+
+const app = express();
+const assestPath = path.join( __dirname, 'public' );
+
+app.use(express.urlencoded({ extended: true }));
+app.use( express.static( assestPath ) );
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use("/new", newMessageRouter);
+app.use("/", indexRouter);
+
+app.listen(3000, () => console.log('Server started on port 3000'));
